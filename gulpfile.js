4b7698bf6c05618ef83ffa6707bb4d85f53cb1b8
Jacob Bare <jacob@limit0.io>
2019-03-05 08:56:05 -0600

    Add lint task with eslint+cached

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,22 @@ const {
   watch,
 } = require('gulp');
 const gulpSass = require('gulp-sass');
+const cache = require('gulp-cached');
+const eslint = require('gulp-eslint');
 
 gulpSass.compiler = require('node-sass');
 
-const sass = () => src('./src/styles/app.scss')
+const lint = () => src('src/**/*.js')
+  .pipe(cache('lint'))
+  .pipe(eslint())
+  .pipe(eslint.format());
+
+const sass = () => src('src/styles/app.scss')
   .pipe(gulpSass().on('error', gulpSass.logError))
   .pipe(dest('./dist'));
 
-const watchSass = () => watch('./src/styles/**/*.scss', sass);
+const watchSass = () => watch('src/styles/**/*.scss', sass);
 
+exports.lint = lint;
 exports.sass = sass;
 exports.watchSass = watchSass;
