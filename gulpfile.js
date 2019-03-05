ced12a3f51a13c60c5a40e8b2f0243ec8aafb4c0
Jacob Bare <jacob@limit0.io>
2019-03-05 08:52:40 -0600

    Add sass watch command

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const {
   dest,
   src,
+  watch,
 } = require('gulp');
 const gulpSass = require('gulp-sass');
 
@@ -10,4 +11,7 @@ const sass = () => src('./src/styles/app.scss')
   .pipe(gulpSass().on('error', gulpSass.logError))
   .pipe(dest('./dist'));
 
+const watchSass = () => watch('./src/styles/**/*.scss', sass);
+
 exports.sass = sass;
+exports.watchSass = watchSass;
