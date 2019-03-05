389adb4ed1d79126a4c653d1a320bd97c0a392f1
Jacob Bare <jacob@limit0.io>
2019-03-05 10:32:27 -0600

    Display package info on boot

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('marko/node-require');
+const pkg = require('../package.json');
 const app = require('./app');
 
 const { log } = console;
@@ -8,7 +9,8 @@ app.get('/', (req, res) => {
   res.json({ ping: 'pong' });
 });
 
+log(`> Starting ${pkg.name} v${pkg.version}...`);
 app.listen(port, () => {
-  log(`Listening on http://0.0.0.0:${port}`);
-  if (process.send) process.send('running');
+  log(`> Server ready on http://0.0.0.0:${port}`);
+  if (process.send) process.send('ready');
 });
